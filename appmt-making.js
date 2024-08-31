1fb391e2b84ab4ab98744b16e2b8c651e1283a0b
SherryH <enjoy.wind7rain@gmail.com>
2024-08-31 13:57:55 +0800

    Finished the part to filling the first 2 pages

--- appmt-making.js
+++ appmt-making.js
@@ -8,10 +8,45 @@ const puppeteer = require('puppeteer');
   // 1. Navigate to the clinic website and log in
   // Go to the Appointment Booking website
   await page.goto('http://59.124.174.66:82/MobileGO.aspx?Code=3807320406');
-  await page.type('#login-email', 'johndoe@example.com');
-  await page.type('#login-password', 'securepassword123');
-  await page.click('#login-submit');
+
+  // sometimes there is a modal with a button to click to remove, sometimes there is not
+  // We use the try...catch {} to handle the case when there is a modal
+  // wait for 10s for Modal to appear, else assume it is already the page
+  try {
+    //The button resides in the following structure
+    /**
+     * <button>
+     *    <span>關閉</span>
+     * </button>
+     */
+
+    // We use the puppeteer xpath to select the button
+    // pseudo css selector "button > span:contains(關閉)" is not a standard css selector so cannot be used
+    await page.click('xpath=//button[span[text()="關閉"]]');
+  } catch (error) {
+    console.log(error);
+    console.log('no modal detected. continuing...');
+  }
+
+  // Now we are on the page, go to 預約掛號
+  const reserveButton = await page.waitForSelector('input[value="預約掛號"]');
+  await reserveButton.click();
   await page.waitForNavigation();
+
+  //Click appmt 點選預約
+  // Tricky part is that there are spaces within the value
+  // We need to trim the space from the value text
+  // xpath=//input[normalize-space(@value) = '點選預約'] -this complex xpath doesnt work
+  /**
+   * <input value="  點選預約  ">
+   */
+  // So we take short cut to select only button with type=submit
+  await page.click("xpath=//input[@type='submit']");
+
+  // Optionally take a screenshot of the confirmation
+  await page.screenshot({ path: 'booking-confirmation.png' });
+
+  //----------------------------------
 
   // 2. Navigate to the booking page
   await page.goto('https://www.abc.com/booking');
@@ -60,3 +95,7 @@ const puppeteer = require('puppeteer');
   // Close the browser
   await browser.close();
 })();
+
+function isNodeList(obj) {
+  return obj instanceof NodeList;
+}
